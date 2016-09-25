08ffb46ea5c2b9512012ea5e6ceeb6d10e631f4d
Sean Manion <seanmanion@Seans-MacBook-Pro.local>
2016-09-25 12:08:42 -0400

    new comment and thing_one.strip

--- more-ruby-practice.rb
+++ more-ruby-practice.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 puts ("What is your most favorite thing?")
-thing_one = gets
+thing_one = gets.strip
 
 puts ("What is your second favorite thing?")
 thing_two = gets.strip
@@ -12,3 +12,5 @@ thing_three = gets.strip
 binding.pry
 
 puts ("These are a few of my favorite things: #{thing_one}, #{thing_two}, #{thing_three}.")
+
+# this is killer
