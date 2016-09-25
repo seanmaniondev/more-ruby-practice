1e9b0e91dbb4cf329e9f1eb597df5c1c5307353c
Sean Manion <seanmanion@Seans-MacBook-Pro.local>
2016-09-25 12:12:44 -0400

    remove binding.pry

--- more-ruby-practice.rb
+++ more-ruby-practice.rb
@@ -9,8 +9,7 @@ thing_two = gets.strip
 puts ("And what's your third favorite thing?")
 thing_three = gets.strip
 
-binding.pry
-
 puts ("These are a few of my favorite things: #{thing_one}, #{thing_two}, #{thing_three}.")
 
 # this is killer
+# remove binding.pry
